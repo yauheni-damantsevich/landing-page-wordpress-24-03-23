6815ab6072c036b88ec6cc638321271855cbcd35
yauheni-damantsevich <yauheni.damantsevich@outlook.com>
2023-03-31 11:37:47 +0300

    change units, add popup

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ import Swiper from "../node_modules/swiper/swiper-bundle.esm.browser.js";
 const swiper1 = new Swiper(".swiper1", {
   slidesPerView: 4,
   loop: true,
-  // loopedSlides: 50,
   navigation: {
     nextEl: ".swiper-button-next1",
     prevEl: ".swiper-button-prev1",
@@ -13,7 +12,6 @@ const swiper1 = new Swiper(".swiper1", {
 const swiper2 = new Swiper(".swiper2", {
   slidesPerView: 4,
   loop: true,
-  // loopedSlides: 50,
   navigation: {
     nextEl: ".swiper-button-next2",
     prevEl: ".swiper-button-prev2",
@@ -23,7 +21,6 @@ const swiper2 = new Swiper(".swiper2", {
 const swiper3 = new Swiper(".swiper3", {
   slidesPerView: 2,
   loop: true,
-  // loopedSlides: 50,
   navigation: {
     nextEl: ".swiper-button-next3",
     prevEl: ".swiper-button-prev3",
@@ -55,9 +52,10 @@ window.addEventListener("load", () => {
   addEvents();
 });
 
+// Videos
+
 const allPlayButtons = document.querySelectorAll(".play");
 const allVideos = document.querySelector(".swiper2");
-
 const addEvents = () => {
   for (let i = 0; i < allPlayButtons.length; i++) {
     allPlayButtons[i].addEventListener("click", togglePlay);
@@ -78,8 +76,6 @@ function togglePlay(event) {
   }
 }
 
-// playButton.addEventListener("click", togglePlay);
-
 function hoverVideo(event) {
   const target = event.currentTarget;
 
@@ -90,4 +86,86 @@ function hoverVideo(event) {
   }
 }
 
-// video.addEventListener("mouseover", hoverVideo);
+// Popup
+
+const appointmentButton = document.querySelector("#appointment");
+const body = document.querySelector("body");
+
+appointmentButton.addEventListener("click", openPopup);
+
+function openPopup(event) {
+  let popupContainer = document.createElement("div");
+  popupContainer.classList.add("popup-container");
+  body.append(popupContainer);
+  let popup = document.createElement("div");
+  popup.classList.add("popup");
+  popupContainer.append(popup);
+  let closeButton = document.createElement("button");
+  closeButton.classList.add("close-button");
+  popup.append(closeButton);
+  let img = document.createElement("img");
+  img.src = "./assets/close.svg";
+  img.alt = "Close";
+  closeButton.append(img);
+  let title = document.createElement("h2");
+  title.textContent = "Онлайн запись";
+  popup.append(title);
+  let text = document.createElement("p");
+  text.textContent =
+    "оставьте свой номер телефона и мы свяжемся с Вами в ближайшее время";
+  popup.append(text);
+  let form = document.createElement("form");
+  popup.append(form);
+  let inputName = document.createElement("input");
+  inputName.type = "text";
+  inputName.placeholder = "Имя";
+  form.append(inputName);
+  let inputTel = document.createElement("input");
+  inputTel.type = "tel";
+  inputTel.placeholder = "Телефон";
+  form.append(inputTel);
+  let submitButton = document.createElement("button");
+  submitButton.type = "submit";
+  submitButton.textContent = "Отправить";
+  submitButton.classList.add("secondary-button");
+  form.append(submitButton);
+  popupContainer.addEventListener("click", closePopupWithBackground);
+  closeButton.addEventListener("click", closePopup);
+  submitButton.addEventListener("click", (event) => {});
+}
+
+function closePopup(event) {
+  const target = event.currentTarget;
+  const popupContainer = target.closest(".popup-container");
+  const closeButton = target.closest(".close-button");
+  closeButton.removeEventListener("click", closePopup);
+  popupContainer.remove();
+}
+
+function closePopupWithBackground(event) {
+  let result = event
+    .composedPath()
+    .find((element) => element.className === "popup");
+  const target = event.currentTarget;
+  const popupContainer = document.querySelector(".popup-container");
+  if (!result) {
+    event.preventDefault();
+    popupContainer.remove();
+  }
+}
+
+function postRequest(event) {
+  event.preventDefault();
+  let form = document.querySelector("form");
+  let formData = new FormData(form);
+  let formDataObject = {};
+  formData.forEach((value, key) => {
+    formDataObject[key] = value;
+  });
+  console.log(formDataObject);
+  closePopup();
+  // fetch
+  showMessage();
+}
+
+function showMessage() {}
